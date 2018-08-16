59fb6f78d06c8b0bd2153f1c6a1f65f8c7a75258
Tania Whiting <tanwhiting@aol.com>
2018-08-16 19:52:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var instruments = ['Guitar','Bass Guitar','Lead Guitar','Drums'];
 
 function theBeatlesPlay(musicians, instruments){
   var arrayN = [];
-  for (var i = 1; i < musicians.length; i++){
+  for (var i = 0; i < musicians.length; i++){
     arrayN.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return arrayN;
