71fb22f50b69d03d1c10ca0612aad46ea2fd459a
Tania Whiting <tanwhiting@aol.com>
2018-08-16 19:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // add solution here
 var musicians = ['John Lennon','Paul McCartney','George Harrison', 'Ringo Starr'];
-var instruments = ['guitar',''
+var instruments = ['guitar','bass guitar','lead guitar','drums'];
+
+function theBeatlesPlay(musicians, instruments){}
