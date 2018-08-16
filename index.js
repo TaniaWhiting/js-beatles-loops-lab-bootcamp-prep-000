d7d17591daa3648aab8635d4aca7fbb7703f54d9
Tania Whiting <tanwhiting@aol.com>
2018-08-16 19:47:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var musicians = ['John Lennon','Paul McCartney','George Harrison', 'Ringo Starr'
 var instruments = ['Guitar','Bass Guitar','Lead Guitar','Drums'];
 
 function theBeatlesPlay(musicians, instruments){
-  var newArray = [];
+  var array = [];
   for (var i = 0; i < musicians.length; i++){
-    newArray.push(musicians[i] + 'plays' + instruments[i]);
+    array.push(musicians[i] + 'plays' + instruments[i]);
   }
-  return newArray;
+  return array;
 }
 
